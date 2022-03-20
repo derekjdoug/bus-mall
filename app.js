49a104ef3cc923034b4ba520ed7496acad959db9
Derek Douglas <derek.j.douglas13@gmail.com>
2022-03-19 20:51:11 -0500

    updated JS functionality to refresh images and add click values while deleting unecessary prototypes

--- app.js
+++ app.js
@@ -9,34 +9,35 @@ let imageTwo = document.getElementById('imageTwo');
 let imageThree = document.getElementById('imageThree');
 
 //Constructor Function
-function Product(name, filePath, views) {
+function Product(name, filePath, views, clicks) {
   this.name = name;
   this.filePath = filePath;
   this.views = views;
+  this.clicks = clicks;
 }
 console.log(Product);
 
 //Products Array
 let productsArr = [
-  new Product('bag', 'img/bag.jpg', 0),
-  new Product('banana', 'img/banana.jpg', 0),
-  new Product('bathroom', 'img/bathroom.jpg', 0),
-  new Product('boots', 'img/boots.jpg', 0),
-  new Product('breakfast', 'img/breakfast.jpg', 0),
-  new Product('bubblegum', 'img/bubblegum.jpg', 0),
-  new Product('chair', 'img/chair.jpg', 0),
-  new Product('cthulhu', 'img/cthulhu.jpg', 0),
-  new Product('dog-duck', 'img/dog-duck.jpg', 0),
-  new Product('dragon', 'img/dragon.jpg', 0),
-  new Product('pen', 'img/pen.jpg', 0),
-  new Product('pet-sweep', 'img/pet-sweep.jpg', 0),
-  new Product('scissors', 'img/scissors.jpg', 0),
-  new Product('shark','img/shark.jpg', 0),
-  new Product('sweep', 'img/sweep.png', 0),
-  new Product('tauntaun', 'img/tauntaun.jpg', 0),
-  new Product('unicorn', 'img/unicorn.jpg', 0),
-  new Product('water-can', 'img/water-can.jpg', 0),
-  new Product('wine-glass', 'img/wine-glass.jpg', 0)
+  new Product('bag', 'img/bag.jpg', 0, 0),
+  new Product('banana', 'img/banana.jpg', 0, 0),
+  new Product('bathroom', 'img/bathroom.jpg', 0, 0),
+  new Product('boots', 'img/boots.jpg', 0, 0),
+  new Product('breakfast', 'img/breakfast.jpg', 0, 0),
+  new Product('bubblegum', 'img/bubblegum.jpg', 0, 0),
+  new Product('chair', 'img/chair.jpg', 0, 0),
+  new Product('cthulhu', 'img/cthulhu.jpg', 0, 0),
+  new Product('dog-duck', 'img/dog-duck.jpg', 0, 0),
+  new Product('dragon', 'img/dragon.jpg', 0, 0),
+  new Product('pen', 'img/pen.jpg', 0, 0),
+  new Product('pet-sweep', 'img/pet-sweep.jpg', 0, 0),
+  new Product('scissors', 'img/scissors.jpg', 0, 0),
+  new Product('shark','img/shark.jpg', 0, 0),
+  new Product('sweep', 'img/sweep.png', 0, 0),
+  new Product('tauntaun', 'img/tauntaun.jpg', 0, 0),
+  new Product('unicorn', 'img/unicorn.jpg', 0, 0),
+  new Product('water-can', 'img/water-can.jpg', 0, 0),
+  new Product('wine-glass', 'img/wine-glass.jpg', 0, 0)
 ];
 console.log(productsArr);
 
@@ -54,31 +55,28 @@ Product.productDisplay = function () {
       product.views++;
     }
   }
-  // console.log(threeProducts);
   return threeProducts;
 };
-// console.log(threeProducts[0]);
-let threeProducts = Product.productDisplay();
 
-// console.log(productsArr);
+let hold = Product.productDisplay();
 
 Product.render = function () {
 
-  let print = threeProducts;
+  hold = Product.productDisplay();
 
   let prodImg1 = document.createElement('img');
-  prodImg1.src = `${print[0].filePath}`;
-  prodImg1.alt = `${print[0].name}`;
+  prodImg1.src = `${hold[0].filePath}`;
+  prodImg1.alt = `${hold[0].name}`;
   prodImg1.id = 'imageOne';
   img1.appendChild(prodImg1);
   let prodImg2 = document.createElement('img');
-  prodImg2.src = `${print[1].filePath}`;
-  prodImg2.alt = `${print[1].name}`;
+  prodImg2.src = `${hold[1].filePath}`;
+  prodImg2.alt = `${hold[1].name}`;
   prodImg2.id = 'imageTwo';
   img2.appendChild(prodImg2);
   let prodImg3 = document.createElement('img');
-  prodImg3.src = `${print[2].filePath}`;
-  prodImg3.alt = `${print[2].name}`;
+  prodImg3.src = `${hold[2].filePath}`;
+  prodImg3.alt = `${hold[2].name}`;
   prodImg3.id = 'imageThree';
   img3.appendChild(prodImg3);
 };
@@ -90,18 +88,17 @@ let userChoice1 = img1;
 userChoice1.addEventListener('click', function (event) {
   event.preventDefault();
 
-  let read = threeProducts;
-  console.log(read);
+  let read = hold;
 
   let row = document.createElement('tr');
   results.appendChild(row);
   let print = document.createElement('td');
-  print.textContent = threeProducts[0].name;
+  print.textContent = read[0].name;
   row.appendChild(print);
-
+  hold[0].clicks++;
   reset();
   Product.render();
-
+  console.log(productsArr);
 });
 
 let userChoice2 = img2;
@@ -109,17 +106,17 @@ let userChoice2 = img2;
 userChoice2.addEventListener('click', function (event) {
   event.preventDefault();
 
-  let read = threeProducts;
-  console.log(read);
+  let read = hold;
 
   let row = document.createElement('tr');
   results.appendChild(row);
   let print = document.createElement('td');
-  print.textContent = threeProducts[1].name;
+  print.textContent = read[1].name;
   row.appendChild(print);
-
+  hold[1].clicks++;
   reset();
   Product.render();
+  console.log(productsArr);
 });
 
 let userChoice3 = img3;
@@ -127,19 +124,19 @@ let userChoice3 = img3;
 userChoice3.addEventListener('click', function (event) {
   event.preventDefault();
 
-  let read = threeProducts;
-  console.log(read);
+  let read = hold;
 
   let row = document.createElement('tr');
   results.appendChild(row);
   let print = document.createElement('td');
   print.textContent = read[2].name;
   row.appendChild(print);
-
+  hold[2].clicks++;
   reset();
   Product.render();
-
+  console.log(productsArr);
 });
+
 
 function reset() {
   while (img1.firstChild, img2.firstChild, img3.firstChild) {
